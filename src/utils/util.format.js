f8a42717700a69823f9fb12ebc084e012c85c5b8
Greg Kindel <gkindel@ramp.com>
2011-12-20 23:47:29 -0500

    search widget

---
+++ src/utils/util.format.js
@@ -0,0 +1,26 @@
+(function () {
+
+    Ramp.Utils.Format = {
+        seconds : function (time) {
+            var zpad = function (val, len) {
+                var r = String(val);
+                while( r.length < len ) {
+                    r = "0" + r;
+                }
+                return r;
+            };
+            var hr = Math.floor(time / 3600);
+            var min = Math.floor( (time %  3600) / 60);
+            var sec = Math.floor(time % 60);
+            var parts = [
+                zpad(min, 2),
+                zpad(sec, 2)
+            ];
+            if( hr )
+                parts.unshift(hr);
+            return parts.join(":");
+        }
+    };
+
+})();
+
