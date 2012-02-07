9bcc66f62c07740da930f949d2c15c3c9ea828b3
Greg Kindel <gkindel@ramp.com>
2012-02-07 15:59:10 -0500

    search gets clear button

--- src/ui/search/ui.search.js
+++ src/ui/search/ui.search.js
@@ -15,7 +15,8 @@
             'searchPlaceholder' : "Search transcript...",
             'ellipsis' : "...",
             'resultsHeader' : "Showing {{count}} {{results}}:",
-            'results' : function (dict) { return "result" + (dict['count'] == 1 ? "" : "s")}
+            'results' : function (dict) { return "result" + (dict['count'] == 1 ? "" : "s")},
+            'clear' : "x"
         }
     };
 
@@ -51,6 +52,7 @@
 
         this.createMarkup();
         this.addListeners();
+        this.clear();
     };
 
     SearchBox.instances = {};
@@ -98,8 +100,17 @@
             sm.append( this.create('submit-label', 'span') );
             f.append(sm);
 
-            this.create('results')
+            var results = this.create('results')
                 .appendTo(c);
+
+            var self = this;
+            this.create("close")
+                .text( this.getString("clear") )
+                .bind("click", function (e){
+                    self.onClose();
+                })
+                .appendTo(c);
+
 
             this.create('tags')
                 .appendTo(c);
@@ -162,8 +173,6 @@
         },
 
 
-
-
         onSearch : function (e) {
             var q = this.find('input').val();
             this.search(q);
@@ -174,9 +183,13 @@
         },
 
         clear : function () {
-            var r = this.find('results');
-            r.empty();
+            var r = this.find('results').empty();
+            this.find('close').hide();
             this.find("tags").show();
+        },
+
+        onClose : function () {
+            this.clear();
         },
 
         onSearchResult : function (e,response) {
@@ -189,6 +202,7 @@
 
             this.find("tags").hide();
 
+            this.find('close').show();
             var r = this.find('results');
 
 
